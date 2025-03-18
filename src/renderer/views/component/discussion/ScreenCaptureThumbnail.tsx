a1f3677bc4e08fb8b8eda1a599bcf1aa24c583f9
Nobuhiro Hayashi <hayashi@humanest.jp>
2025-03-18 17:18:55 +0900

    Add tooltip to ScreenCaptureThumbnail for timestamp display
    Signed-off-by: Nobuhiro Hayashi <hayashi@humanest.jp>

--- src/renderer/views/component/discussion/ScreenCaptureThumbnail.tsx
+++ src/renderer/views/component/discussion/ScreenCaptureThumbnail.tsx
@@ -2,6 +2,8 @@ import { useMemo } from "react";
 import { useMinutesStore } from "../../store/useMinutesStore.jsx";
 import { useVBStore } from "../../store/useVBStore.jsx";
 import { useDetailViewDialog } from "../common/useDetailViewDialog.jsx";
+import { Tooltip } from "@mui/material";
+import { secondsToHMS } from "../../../util.js";
 
 // スクリーンキャプチャのサムネイルを表示するコンポーネント
 
@@ -31,7 +33,13 @@ export const ScreenCaptureThumbnail = ({
   }, [allCapturedScreens, timestampSec, durationMSec]);
 
   if (capturedScreens.length === 0) {
-    return <></>;
+    return (
+      <div className="flex flex-col gap-1 mr-4">
+        <div className="w-12 h-12 flex items-center justify-center">
+          <span>&#8203;</span>
+        </div>
+      </div>
+    );
   }
 
   // サムネイルのクリックで該当のスクリーンキャプチャを表示
@@ -39,30 +47,34 @@ export const ScreenCaptureThumbnail = ({
     useDetailViewDialog();
 
   return (
-    <div className="flex flex-col gap-1 mr-2">
-      {capturedScreens.map((latestCapture, index) => (
+    <div className="flex flex-col gap-1 mr-4">
+      {capturedScreens.map((frame, index) => (
         <div className="w-12 h-auto" key={index}>
-          <img
-            src={`file://${latestCapture.filePath}`}
-            alt="Screen capture"
-            className="w-full object-contain border border-gray-300 rounded"
-            onClick={() => {
-              detailViewDialog({
-                content: (
-                  <img
-                    src={`file://${latestCapture.filePath}`}
-                    alt="Screen capture"
-                    className="w-full object-contain"
-                  />
-                ),
-                onClose: handleClose,
-                dialogConf: {
-                  fullWidth: true,
-                  maxWidth: "lg",
-                },
-              });
-            }}
-          />
+          <Tooltip
+            title={secondsToHMS((frame.timestamp - startTimestamp) / 1000)}
+          >
+            <img
+              src={`file://${frame.filePath}`}
+              alt="Screen capture"
+              className="w-full object-contain border border-gray-300 rounded"
+              onClick={() => {
+                detailViewDialog({
+                  content: (
+                    <img
+                      src={`file://${frame.filePath}`}
+                      alt="Screen capture"
+                      className="w-full object-contain"
+                    />
+                  ),
+                  onClose: handleClose,
+                  dialogConf: {
+                    fullWidth: true,
+                    maxWidth: "lg",
+                  },
+                });
+              }}
+            />
+          </Tooltip>
         </div>
       ))}
       {renderDetailViewDialog()}
